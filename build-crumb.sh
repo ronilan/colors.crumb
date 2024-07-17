659de043ac9970e0f0cc369fe2660649acc61543
Ron Ilan <ronilan.webdev@gmail.com>
2024-07-16 21:22:23 -0700

    Updated build script to allow build from branch name

--- build-crumb.sh
+++ build-crumb.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
-default="v0.0.1"
+default="v0.0.2"
 checkout=${1:-$default}
 into=".tmp~crumb~inflate"
 
 build_and_clean() {
-  gcc src/*.c -Wall -lm -o crumb
+
+if gcc src/*.c -Wall -lm -o crumb;
+then
+  printf "\e[32mCrumb built successfully\e[0m\n"
+else
+  printf "\e[31mCrumb built failed\e[0m\n"
+  exit 1
+fi
   cd ..
   mv ./$into/crumb crumb
   rm -rf $into
@@ -17,14 +24,19 @@ then
   cd $into || exit
   git reset --hard "$checkout"
   build_and_clean
-  printf "\e[32mCrumb built successfully from %s\e[0m\n" "$checkout"
-elif [ "$(printf '%s' "$checkout" | cut -c 1)" = "v" ] # its is a sha
+elif [ "$(printf '%s' "$checkout" | cut -c 1)" = "v" ] # it is a tag
 then # assumed a version tag
   printf "\e[32mBuilding version %s \e[0m \n" "$checkout"
   git clone -c advice.detachedHead=false --branch "$checkout" --depth 1 https://github.com/liam-ilan/crumb.git $into
   cd $into || exit
   build_and_clean
-  printf "\e[32mCrumb version %s built successfully\e[0m\n" "$checkout"
-else
-  printf "\e[31m%s not valid version tag nor SHA \e[0m\n" "$checkout"
+else ## try branch
+  printf "\e[32mBuilding version %s \e[0m \n" "$checkout"
+  if git clone -c advice.detachedHead=false --branch "$checkout" --depth 1 https://github.com/liam-ilan/crumb.git $into
+  then
+    cd $into || exit
+    build_and_clean
+  else
+    printf "\e[31m%s not valid version tag nor branch name nor SHA \e[0m\n" "$checkout"
+  fi
 fi
